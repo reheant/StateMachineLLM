59e752522c722c1660a3173b152a6575fb20a2d4
Alec Tufenkjian <alectufenkjian@gmail.com>
2024-10-03 19:27:17 -0400

    Added functionnality to communicate with gpt4 api

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
+import os
+from openai import AsyncOpenAI
+
 import chainlit as cl
 
+api_key = os.environ.get("OPENAI_API_KEY")
+client = AsyncOpenAI(api_key=api_key)
+
+cl.instrument_openai()
+
+@cl.on_chat_start
+def start_chat():
+    cl.user_session.set("message_history", [{"role": "system", "content": "You are a helpful assistant."}])
+
+async def call_gpt4(message_history):
+    settings = { "model": "gpt-4" }
+    stream = await client.chat.completions.create(messages=message_history, stream=True, **settings)
+    final_answer = cl.Message(content="", author="Answer")
+
+    async for part in stream:
+        new_delta = part.choices[0].delta
+ 
+        if new_delta.content:
+            if not final_answer.content:
+                await final_answer.send()
+            await final_answer.stream_token(new_delta.content)
+
+    if final_answer.content:
+        await final_answer.update()
 
 @cl.on_message
-async def main(message: cl.Message):
-    await cl.Message(
-        content=f"Received: {message.content}",
-    ).send()
+async def run_conversation(message: cl.Message):
+    message_history = cl.user_session.get("message_history")
+    message_history.append({"name": "user", "role": "user", "content": message.content})
+
+    message = await call_gpt4(message_history)
+    await cl.Message(content=message.content, author="Answer").send()
