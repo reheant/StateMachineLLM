6f85a7c526335a482655b604baa02e6d88bb3d5d
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-10-24 12:31:00 -0700

    Change naming convention of table helper functions

--- util.py
+++ util.py
@@ -13,7 +13,7 @@ def call_gpt4(prompt, model="gpt-4", max_tokens=150, temperature=0.7):
 
     return response.choices[0].message.content
 
-def extract_html_tables_from_llm_response(llm_response):
+def extract_html_tables(llm_response):
     # extract the content within the ```html code blocks using regex
     code_blocks = re.findall(r'```html(.*?)```', llm_response, re.DOTALL)
     
@@ -33,7 +33,7 @@ def extract_html_tables_from_llm_response(llm_response):
     
     return tables
 
-def extract_table_using_headers_from_llm_response(llm_response, headers):
+def extract_table_using_headers(llm_response, headers):
     
     # find all tables in the response
     tables = extract_html_tables_from_llm_response(llm_response=llm_response)
@@ -46,19 +46,19 @@ def extract_table_using_headers_from_llm_response(llm_response, headers):
     
     return None
 
-def extract_states_events_table_from_llm_response(llm_response):
+def extract_states_events_table(llm_response):
     states_events_table_headers = ["Current State", "Event", "Next State(s)"]
     states_events_table = extract_table_using_headers_from_llm_response(llm_response=llm_response,
                                                                         headers=states_events_table_headers)
     return states_events_table
 
-def extract_parallel_states_table_from_llm_response(llm_response):
+def extract_parallel_states_table(llm_response):
     parallel_states_table_headers = ["Parallel State", "Substate", "Reference from Problem Description"]
     parallel_states_table = extract_table_using_headers_from_llm_response(llm_response=llm_response,
                                                                           headers=parallel_states_table_headers)
     return parallel_states_table
 
-def extract_transitions_guards_table_from_llm_response(llm_response):
+def extract_transitions_guards_table(llm_response):
     transitions_guards_table_headers = ["From State", "To State", "Event", "Guard"]
     transitions_guards_table = extract_table_using_headers_from_llm_response(llm_response=llm_response,
                                                                              headers=transitions_guards_table_headers)
