8c75e6984480c925024b846fedcb7e6d08aa0722
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-10-28 13:44:05 -0700

    Update ActionSearchAction table format to remove Entry and Exit Actions

--- actions/ActionSearchAction.py
+++ actions/ActionSearchAction.py
@@ -16,18 +16,23 @@ class ActionSearchAction(BaseAction):
 
         prompt = f"""
                      You are an AI assistant specialized in creating UML state machines. You will be provided an HTML table containing information related to the transitions of a UML state machine along with a problem description.
-                     Your task is to update the transition table with Entry Actions and Exit Actions identified from the problem description. Provide updates to the provided HTML transition table by including two new columns for the Entry Action (The action performed on the To State after the transition), and the Exit Action (The action performed on the From State before the transition). 
-                     The HTML table format should be as follows:
+                     Your task is to update the transition table to include an Action for each transition. Entry Actions and Exit Actions identified from the problem description. Provide updates to the provided HTML transition table by including a new column for Action. 
+                     
+                     The HTML table format MUST be as follows. You are NOT allowed to add extra columns or entries into the updated transitions table.
+                     
                      ```html <table border="1"> 
-                     <tr> <th>From State</th> <th>To State</th> <th>Event</th> <th>Guard</th> <th>Entry Action</th> <th>Exit Action</th> </tr> 
-                     <tr> <td rowspan="3"> State1 </td> <td> State2 </td> <td> Event1 </td> <td> Condition1 </td> <td> Entry Action 1 </td> <td> Entry Action 2 </td> </tr> 
-                     <tr> <td rowspan="3"> State2 </td> <td> State3 </td> <td> Event1 </td> <td> Condition1 </td> <td> NONE </td> <td> NONE </td> </tr> 
+                     <tr> <th>From State</th> <th>To State</th> <th>Event</th> <th>Guard</th> <th>Action</th> </tr> 
+                     <tr> <td rowspan="3"> State1 </td> <td> State2 </td> <td> Event1 </td> <td> Condition1 </td> <td> Action 1 </td> </tr> 
+                     <tr> <td rowspan="3"> State2 </td> <td> State3 </td> <td> Event1 </td> <td> Condition1 </td> <td> NONE </td> </tr> 
                      </table> ```
-                     If the transition does not have an Entry Action or Exit Action, leave the action entry in the table blank.
+                     
+                     If the transition does not have an Action, enter NONE into the table.
                      The definition of an action in a UML state machine is described below:
                      When an event instance is dispatched, the state machine responds by performing actions, such as changing a variable, performing I/O, invoking a function, generating another event instance, or changing to another state. Any parameter values associated with the current event are available to all actions directly caused by that event.
+                     
                      The transition table is: 
                      {transition_table}
+                     
                      The problem description is: 
                      {system_description}
                   """
