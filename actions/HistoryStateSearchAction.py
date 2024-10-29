d440689d8b587f51c259f6453150a76275ddb0ec
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-10-28 18:16:45 -0700

    Fix table parser and merging for HistoryStateSearchAction
    Co-authored-by: Karl Bridi <Kalamar136@users.noreply.github.com>

--- actions/HistoryStateSearchAction.py
+++ actions/HistoryStateSearchAction.py
@@ -1,5 +1,6 @@
 from sherpa_ai.actions.base import BaseAction
 from util import call_gpt4, extract_history_state_table, appendTables, addColumn, extractColumn
+from bs4 import Tag
 
 class HistoryStateSearchAction(BaseAction):
     name: str = "history_state_search_action"
@@ -10,10 +11,10 @@ class HistoryStateSearchAction(BaseAction):
 
     def execute(self, description):
         print(f"Running {self.name}...")
-        hierarchical_states, tmp = self.belief.get('hierarchical_state_search_action')
-        print(hierarchical_states)
-        hierarchical_states = extractColumn(hierarchical_states, 0)
+        hierarchical_states_table, transition_table = self.belief.get('hierarchical_state_search_action')
+        hierarchical_states = list(set(extractColumn(hierarchical_states_table, 0)))
         modeled_system, tmp = self.belief.get('state_event_search_action')
+        areThereHistoryStates = False
 
         for i in range(len(hierarchical_states)):
             prompt = f'''
@@ -28,23 +29,18 @@ class HistoryStateSearchAction(BaseAction):
 
             If the hierarchical state doesn't require a history state, output NONE.
             Otherwise, output your answer in the following HTML form with a row per transition to the history state:
-            ```html <table border="1"> <tr> <th>From State</th> <th>Event</th> <th>Guard</th> </tr>
-            <tr> <td rowspan="3"> State1 </td> <td> Event1 </td> <td> Condition1 </td> </tr>
-            <tr> <td rowspan="3"> State3 </td> <td> Event2 </td> <td> NONE </td> </tr> </table> ```
+            ```html <table border="1"> <tr> <th>From State</th> <th>Event</th> <th>Guard</th> <th>Action</th> </tr>
+            <tr> <td rowspan="3"> State1 </td> <td> Event1 </td> <td> Condition1 </td> <td> NONE </td> </tr>
+            <tr> <td rowspan="3"> State3 </td> <td> Event2 </td> <td> NONE </td> </tr> <td> Action2 </td> </table> ```
             '''
             answer = call_gpt4(prompt)
-            print(answer)
-            history_state_table = None
-            if not history_state_table:
-                raw_table = extract_history_state_table(answer, True)
-                print(raw_table)
-                if raw_table:
-                    history_state_table = addColumn(raw_table, 'To State', 1, hierarchical_states[i])
-            else:
-                raw_table = extract_history_state_table(answer, False)
-                print(raw_table)
-                if raw_table:
-                    tmp = addColumn(raw_table, None, 1, hierarchical_states[i])
-                    history_state_table = appendTables(history_state_table, tmp)
-
-        return history_state_table
+            raw_table = extract_history_state_table(answer, False)
+            print(raw_table)
+            if raw_table:
+                tmp = addColumn(raw_table, None, 1, f'{hierarchical_states[i]}.H')
+                print(tmp)
+                history_state_table = tmp if not areThereHistoryStates else appendTables(history_state_table, tmp)
+                areThereHistoryStates = True
+        
+        return_table = appendTables(transition_table, history_state_table) if areThereHistoryStates else transition_table
+        return return_table
