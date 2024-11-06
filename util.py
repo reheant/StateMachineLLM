58943fd99165a81058469864c0b1fdbc730fa954
Kalamar136 <karl.bridi@mail.mcgill.ca>
2024-11-06 18:43:46 -0500

    Set the temperature to 0.7 which experimentally seems to be the best temperature to use

--- util.py
+++ util.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup, Tag
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-def call_gpt4(prompt, model="gpt-4o", max_tokens=1200, temperature=0.3):
+def call_gpt4(prompt, model="gpt-4o", max_tokens=1200, temperature=0.7):
     response = openai.chat.completions.create(
         model=model,
         messages=[{"role": "user", "content": prompt}],
