99f422156b278608bb6e5a8a1858c35a3ae54981
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-11-23 12:41:49 -0500

    Rename Thermomix description to example_description in event_driven_smf

--- event_driven_smf.py
+++ event_driven_smf.py
@@ -20,61 +20,61 @@ from actions.EventDrivenFilterTransitionsAction import EventDrivenFilterTransiti
 from actions.EventDrivenHistoryStateSearchAction import EventDrivenHistoryStateSearchAction
 from event_driven_smf_transitions import transitions
 
-description = """
-              The Thermomix TM6 is an all-in-one kitchen appliance that
-              preps ingredients and cooks them to perfection.
-              On delivery, the Thermomix TM6 is set to transportation
-              mode. When the selector (button) is pressed to start up the
-              Thermomix TM6 for the first time, the transportation
-              mode is automatically deactivated, and the home screen is
-              shown. To turn the Thermomix TM6 off, hold the selector
-              down for at least five seconds until a message appears to
-              confirm that the Thermomix TM6 is switching off. You can
-              then release the selector. If the Thermomix TM6 has been
-              turned off, pressing the selector turns it back on and the
-              home screen is shown. To save energy, the Thermomix TM6 switches off automatically after 15
-              minutes when not in use. A message appears for the last 30 seconds, allowing automatic shutdown to
-              be canceled and the home screen to be shown (by selecting cancel on the appliance's screen or by
-              removing the cooking bowl).
-              To cook a meal, select a recipe on the screen and then select start to follow the step-by-step
-              instructions. First, add ingredients as instructed. The integrated scale weighs them and allows the
-              next step only if the correct amount has been added. Continue to the next step by selecting next on
-              the screen. The Thermomix TM6 chops the ingredients for as long as and at the speed required for
-              the recipe. When the chopping step is done, select next for the Thermomix TM6 to start the cooking
-              step. Again, the Thermomix TM6 cooks the meal at the temperature and time required for the
-              recipe. At the end of any recipe step, the Thermomix TM6 may prompt you to add further
-              ingredients, which are then again weighed, chopped, and cooked. After the last step, the
-              Thermomix TM6 informs you that the meal is ready to be served. When the cooking bowl is
-              removed, the Thermomix TM6 returns to the home screen. It is not possible to cook a meal if the
-              cooking bowl is not correctly placed on the Thermomix TM6.
-              """
+example_description = """
+                    The Thermomix TM6 is an all-in-one kitchen appliance that
+                    preps ingredients and cooks them to perfection.
+                    On delivery, the Thermomix TM6 is set to transportation
+                    mode. When the selector (button) is pressed to start up the
+                    Thermomix TM6 for the first time, the transportation
+                    mode is automatically deactivated, and the home screen is
+                    shown. To turn the Thermomix TM6 off, hold the selector
+                    down for at least five seconds until a message appears to
+                    confirm that the Thermomix TM6 is switching off. You can
+                    then release the selector. If the Thermomix TM6 has been
+                    turned off, pressing the selector turns it back on and the
+                    home screen is shown. To save energy, the Thermomix TM6 switches off automatically after 15
+                    minutes when not in use. A message appears for the last 30 seconds, allowing automatic shutdown to
+                    be canceled and the home screen to be shown (by selecting cancel on the appliance's screen or by
+                    removing the cooking bowl).
+                    To cook a meal, select a recipe on the screen and then select start to follow the step-by-step
+                    instructions. First, add ingredients as instructed. The integrated scale weighs them and allows the
+                    next step only if the correct amount has been added. Continue to the next step by selecting next on
+                    the screen. The Thermomix TM6 chops the ingredients for as long as and at the speed required for
+                    the recipe. When the chopping step is done, select next for the Thermomix TM6 to start the cooking
+                    step. Again, the Thermomix TM6 cooks the meal at the temperature and time required for the
+                    recipe. At the end of any recipe step, the Thermomix TM6 may prompt you to add further
+                    ingredients, which are then again weighed, chopped, and cooked. After the last step, the
+                    Thermomix TM6 informs you that the meal is ready to be served. When the cooking bowl is
+                    removed, the Thermomix TM6 returns to the home screen. It is not possible to cook a meal if the
+                    cooking bowl is not correctly placed on the Thermomix TM6.
+                    """
 
 belief = Belief()
-belief.set("description", description)
+belief.set("description", example_description)
 event_driven_system_name_search_action = EventDrivenSystemNameSearchAction(belief=belief,
-                                                                           description=description)
+                                                                           description=example_description)
 event_driven_state_search_action = EventDrivenStateSearchAction(belief=belief,
-                                                                description=description)
+                                                                description=example_description)
 event_driven_initial_state_search_action = EventDrivenInitialStateSearchAction(belief=belief,
-                                                                               description=description)
+                                                                               description=example_description)
 event_driven_event_search_action = EventDrivenEventSearchAction(belief=belief,
-                                                                description=description)
+                                                                description=example_description)
 event_driven_associate_events_with_states = EventDrivenAssociateEventsWithStatesAction(belief=belief,
-                                                                                       description=description)
+                                                                                       description=example_description)
 event_driven_create_transitions_action = EventDrivenCreateTransitionsAction(belief=belief,
-                                                                            description=description)
+                                                                            description=example_description)
 event_driven_filter_transitions_action = EventDrivenFilterTransitionsAction(belief=belief,
-                                                                            description=description)
+                                                                            description=example_description)
 event_driven_create_hierarchical_states_action = EventDrivenCreateHierarchicalStatesAction(belief=belief,
-                                                                                           description=description)
+                                                                                           description=example_description)
 event_driven_hierarchical_initial_state_search_action = EventDrivenHierarchicalInitialStateSearchAction(belief=belief,
-                                                                                                        description=description)
+                                                                                                        description=example_description)
 event_driven_refactor_transition_names_action =  EventDrivenRefactorTransitionNamesAction(belief=belief,
-                                                                                          description=description)
+                                                                                          description=example_description)
 event_driven_history_state_search_action =  EventDrivenHistoryStateSearchAction(belief=belief,
-                                                                                          description=description)
+                                                                                          description=example_description)
 event_driven_display_results_action = EventDrivenDisplayResultsAction(belief=belief,
-                                                                      description=description)
+                                                                      description=example_description)
 
 event_driven_action_map = {
     event_driven_system_name_search_action.name: event_driven_system_name_search_action,
