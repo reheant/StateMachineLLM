ca7718ec9d66314b4d41289b05cb8a4e046d1646
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-12-24 15:52:26 -0800

    Add log_file_path param to EventDrivenDisplayResultsAction

--- backend/event_driven_smf/actions/EventDrivenDisplayResultsAction.py
+++ backend/event_driven_smf/actions/EventDrivenDisplayResultsAction.py
@@ -1,6 +1,7 @@
-from sherpa_ai.actions.base import BaseAction
+from resources.SMFAction import SMFAction
+from resources.environmental_impact.impact_tracker import tracker
 
-class EventDrivenDisplayResultsAction(BaseAction):
+class EventDrivenDisplayResultsAction(SMFAction):
     """
     The EventDrivenDisplayResultsAction displays all tables at the end of
     the state machine creation process: the table of all states, the initial
@@ -11,6 +12,7 @@ class EventDrivenDisplayResultsAction(BaseAction):
     name: str = "event_driven_display_results_action"
     args: dict = {}
     usage: str = "Display the results of the SMF"
+    log_file_path: str = ""
 
     def execute(self):
         """
@@ -18,25 +20,25 @@ class EventDrivenDisplayResultsAction(BaseAction):
         of the UML state machine to the console
         """
         
-        print(f"Running {self.name}...")
+        self.log(f"Running {self.name}...")
 
         event_driven_hierarchical_states_table = self.belief.get("event_driven_create_hierarchical_states_action")
-        print(f"Hierarchical States Table:\n{event_driven_hierarchical_states_table}")
+        self.log(f"Hierarchical States Table:\n{event_driven_hierarchical_states_table}")
 
         event_driven_initial_state = self.belief.get("event_driven_initial_state_search_action")
-        print(f"Initial System State: {event_driven_initial_state}")
+        self.log(f"Initial System State: {event_driven_initial_state}")
 
         event_driven_hierarchical_initial_states = self.belief.get("event_driven_hierarchical_initial_state_search")
-        print(f"Hierarchical Initial States:\n{event_driven_hierarchical_initial_states}")
+        self.log(f"Hierarchical Initial States:\n{event_driven_hierarchical_initial_states}")
 
         event_driven_transitions = self.belief.get("event_driven_history_state_search_action")
-        print(f"Transitions:\n{event_driven_transitions}")
+        self.log(f"Transitions:\n{event_driven_transitions}")
 
         transitionsParallelRegionsTuple = self.belief.get('event_driven_parallel_regions_search_action')
-        print(f"Parallel Regions:\n{transitionsParallelRegionsTuple[1]}")
-        print("\nEnvironmental Assessment:")
-        print(f"Total Completion Tokens: {tracker.total_completion_tokens}")
-        print(f"Total Carbon Emissions: {tracker.carbon_emissions} kgCO2eq")
-        print(f"Total Energy Use: {tracker.energy_consumed} kWh")
-        print(f"Total Abiotic Resource Use: {tracker.abiotic_resource_depletion} gSbeq")
-        print("\n")
+        self.log(f"Parallel Regions:\n{transitionsParallelRegionsTuple[1]}")
+        self.log("\nEnvironmental Assessment:")
+        self.log(f"Total Completion Tokens: {tracker.total_completion_tokens}")
+        self.log(f"Total Carbon Emissions: {tracker.carbon_emissions} kgCO2eq")
+        self.log(f"Total Energy Use: {tracker.energy_consumed} kWh")
+        self.log(f"Total Abiotic Resource Use: {tracker.abiotic_resource_depletion} gSbeq")
+        self.log("\n")
