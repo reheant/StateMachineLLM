e3aed04b2020eb0488ef6df38455b63080ecffdb
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-12-24 15:53:34 -0800

    Add log_file_path param to EventDrivenHierarchicalInitialStateSearchAction

--- backend/event_driven_smf/actions/EventDrivenHierarchicalInitialStateSearchAction.py
+++ backend/event_driven_smf/actions/EventDrivenHierarchicalInitialStateSearchAction.py
@@ -1,10 +1,10 @@
-from sherpa_ai.actions.base import BaseAction
+from resources.SMFAction import SMFAction
 from resources.n_shot_examples_event_driven import get_n_shot_examples
 from resources.util import call_llm, group_parent_child_states
 from resources.n_shot_examples_event_driven import get_n_shot_examples
 import re
 
-class EventDrivenHierarchicalInitialStateSearchAction(BaseAction):
+class EventDrivenHierarchicalInitialStateSearchAction(SMFAction):
     """
     The EventDrivenHierarchicalInitialStateSearchAction prompts the LLM 
     to identify the initial state of each hierarchical state, as identified by the
@@ -14,6 +14,7 @@ class EventDrivenHierarchicalInitialStateSearchAction(BaseAction):
     args: dict = {}
     usage: str = "Given a description of a system and the Hierarchical States of the system, identify the initial state of each Hierarchical State in the UML state machine of the system."
     description: str = ""
+    log_file_path: str = ""
 
     def identify_initial_state(self, parent_state, child_states):
         """
@@ -25,39 +26,39 @@ class EventDrivenHierarchicalInitialStateSearchAction(BaseAction):
         formatted_child_states = ", ".join(child_states)
 
         prompt = f"""
-You are an expert requirements engineer specializing in designing UML state machines from textual descriptions of systems. Your task is to determine the initial state of a superstate (or whether the superstate doesn't have an initial state).
+                You are an expert requirements engineer specializing in designing UML state machines from textual descriptions of systems. Your task is to determine the initial state of a superstate (or whether the superstate doesn't have an initial state).
 
-Here's the information for the system you need to analyze:
+                Here's the information for the system you need to analyze:
 
-<system_description>
-{self.description}
-</system_description>
+                <system_description>
+                {self.description}
+                </system_description>
 
-<system_name>
-{system_name}
-</system_name>
+                <system_name>
+                {system_name}
+                </system_name>
 
-superstate_inspected:
-<superstate_inspected>{parent_state}</superstate_inspected>
+                superstate_inspected:
+                <superstate_inspected>{parent_state}</superstate_inspected>
 
-substates_inspected:
-<substates_inspected>{formatted_child_states}</substates_inspected>
+                substates_inspected:
+                <substates_inspected>{formatted_child_states}</substates_inspected>
 
-Your objective is to determine the initial state of the superstate inspected. Follow these steps:
+                Your objective is to determine the initial state of the superstate inspected. Follow these steps:
 
-1. Carefully analyze the provided system description, name, superstate provided, and associated substates.
-2. For the provided superstate and substates, determine which substate is the Initial State of its superstate. Your answer MUST be in the following format:
+                1. Carefully analyze the provided system description, name, superstate provided, and associated substates.
+                2. For the provided superstate and substates, determine which substate is the Initial State of its superstate. Your answer MUST be in the following format:
 
-<superstate_initial_state>InitialState</superstate_initial_state>
+                <superstate_initial_state>InitialState</superstate_initial_state>
 
-{get_n_shot_examples(['printer_winter_2017'],['system_name', 'system_description', 'superstate_inspected', 'substates_inspected', 'superstate_initial_state'])}
+                {get_n_shot_examples(['printer_winter_2017'],['system_name', 'system_description', 'superstate_inspected', 'substates_inspected', 'superstate_initial_state'])}
 
-Remember, your expertise in UML state machines is crucial for creating an accurate and efficient hierarchical design. The quality of your work will directly impact the success of the system's implementation. Take pride in your role as a key contributor to this important project.
-        The formatting must follow the example precisely. The initial state must be between quotation marks "". 
-        Otherwise your family will starve to death.
-"""
+                Remember, your expertise in UML state machines is crucial for creating an accurate and efficient hierarchical design. The quality of your work will directly impact the success of the system's implementation. Take pride in your role as a key contributor to this important project.
+                        The formatting must follow the example precisely. The initial state must be between quotation marks "". 
+                        Otherwise your family will starve to death.
+                """
 
-        print(prompt)
+        self.log(prompt)
         response = call_llm(prompt=prompt,
                              temperature=0.7)
         
@@ -67,7 +68,7 @@ Remember, your expertise in UML state machines is crucial for creating an accura
             superstate_initial_state = superstate_initial_state_search.group(1).strip('\'"')
         else:
             superstate_initial_state = "NOT FOUND"
-        print(superstate_initial_state)
+        self.log(superstate_initial_state)
 
         return superstate_initial_state
 
@@ -77,7 +78,7 @@ Remember, your expertise in UML state machines is crucial for creating an accura
         map the parent states to a list of its child states, and prompts the LLM through the identify_initial_state()
         function to find the initial state of each parent hierarchical state
         """
-        print(f"Running {self.name}...")
+        self.log(f"Running {self.name}...")
 
         event_driven_hierarchical_state_table = self.belief.get("event_driven_create_hierarchical_states_action")
 
@@ -92,5 +93,5 @@ Remember, your expertise in UML state machines is crucial for creating an accura
 
             initial_states_dict[parent_state] = initial_state
 
-        print(initial_states_dict)
+        self.log(initial_states_dict)
         return initial_states_dict
