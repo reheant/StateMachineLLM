3d861c05a72cff5d3c2f18e4de10b1c2c2bf591a
Kalamar136 <karl.bridi@mail.mcgill.ca>
2025-01-16 19:18:49 -0500

    Fix for history state step error

--- backend/event_driven_smf/actions/EventDrivenHistoryStateSearchAction.py
+++ backend/event_driven_smf/actions/EventDrivenHistoryStateSearchAction.py
@@ -28,7 +28,7 @@ class EventDrivenHistoryStateSearchAction(SMFAction):
         
         transitions_table = self.belief.get('event_driven_factor_out_transitions_for_hierarchal_states_action')
         modeled_system = self.belief.get('event_driven_system_name_search_action')
-        superstates = [state for state in gsm_tables_to_dict(self.belief.get('event_driven_create_hierarchical_states_action'), transitions_table, None)[0] if isinstance(state, dict)]
+        superstates = [state for state in gsm_tables_to_dict(self.belief.get('event_driven_create_hierarchical_states_action'), None, None, nesting = False)[0] if isinstance(state, dict)]
         events = self.belief.get('event_driven_event_search_action')
         n_shot_example_list = self.belief.get("n_shot_examples")
 
